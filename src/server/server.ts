e43f5bb0fa544d9e836dedfbef3cef6546d32447
Kyle Slugg <kyle.slugg@gmail.com>
2023-07-25 16:49:10 -0400

    Playing around with integrating Vite and Express servers

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response, Errback } from 'express';
+import express, { Request, Response, Errback, NextFunction } from 'express';
+import ViteExpress from 'vite-express';
 import SpotifyRouter from './routes/spotifyRouter.ts';
 import { configDotenv } from 'dotenv';
 import * as url from 'url';
@@ -10,22 +11,37 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const PORT = 3000;
-const app = express();
+export const app = express();
+
+// if (!process.env['VITE']) {
+//   const frontendFiles = process.cwd() + '/dist'
+//   app.use(express.static(frontendFiles))
+//   app.get('/*', (_, res) => {
+//     res.send(frontendFiles + '/index.html')
+//   })
+//   app.listen(process.env['PORT'])
+// }
 
 app.use('/spotify', SpotifyRouter);
 
 app.use('/auth', AuthenticationRouter);
 
+app.use(ViteExpress.static());
+
 app.use(express.static(url.resolve(__dirname, '../assets')));
 
-// Catch-all error handler
-app.use('*', (req: Request, res: Response) => {
-  return res.status(404).send("This is not the page you're looking for...");
-});
+// app.get('/*', (req, res) => {
+//   res.render(url.resolve(__dirname, '../../views/index.html.ejs'));
+// });
+
+// // Catch-all error handler
+// app.use('*', (req: Request, res: Response) => {
+//   return res.status(404).send("This is not the page you're looking for...");
+// });
 
 // Global error handling middleware
 
-app.use((err: Errback, req: Request, res: Response) => {
+app.use((err: Errback, req: Request, res: Response, next: NextFunction) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
     status: 400,
@@ -36,8 +52,8 @@ app.use((err: Errback, req: Request, res: Response) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// ViteExpress.listen(app, PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
 
 export default app;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import express from './express-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), express('src/server/server.ts')]
+});
